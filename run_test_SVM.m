7a5307235c802f00b67829f7b2f90f540e2175e4
rwttr <rattachai.gov@gmail.com>
2018-01-04 19:25:38 +0700

    Test(Eval) Value
    Done

--- run_test_SVM.m
+++ run_test_SVM.m
@@ -1,5 +1,9 @@
 % response Vector = test_responseVec
 % all-feature model shapeF:textFRR:textCMT:textHOG
+run_extFeature;
+run_extTestFeature;
+run_SVM;
+
 nRowPos = size(test_shF_Pos,1);
 nRowNeg = size(test_shF_Neg,1);
 nCol = size(test_shF_Pos,2)+size(test_teF_FRR_Pos,2)+...
@@ -16,22 +20,54 @@ X((nRowPos+1):end,:) = ...
 [label_G,score_G] = predict(SVM_4gaussian,X);
 [label_P,score_P] = predict(SVM_4poly,X);
 
+%------------------------------------------------------------
+%Test Score part - for each SVM Kernel
+%------------------------------------------------------------
 %True positive (Accept true samples):raw value
-sum(label_L.*Y)
-sum(label_G.*Y)
-sum(label_P.*Y)
+Tp_4L = sum(label_L.*Y);
+Tp_4G = sum(label_G.*Y);
+Tp_4P = sum(label_P.*Y);
 
 %True Negaitive (Reject false samples):raw value
-sum(imcomplement(label_L).*imcomplement(Y));
-sum(imcomplement(label_G).*imcomplement(Y));
-sum(imcomplement(label_P).*imcomplement(Y));
+Tn_4L = sum(imcomplement(label_L).*imcomplement(Y));
+Tn_4G = sum(imcomplement(label_G).*imcomplement(Y));
+Tn_4P = sum(imcomplement(label_P).*imcomplement(Y));
 
 %False Positive (Accept false samples):raw value
-sum(label_L.*imcomplement(Y));
-sum(label_G.*imcomplement(Y));
-sum(label_P.*imcomplement(Y));
+Fp_4L = sum(label_L.*imcomplement(Y));
+Fp_4G = sum(label_G.*imcomplement(Y));
+Fp_4P = sum(label_P.*imcomplement(Y));
 
 %False Negative (Reject true samples):raw value
-sum(imcomplement(label_L).*Y);
-sum(imcomplement(label_G).*Y);
-sum(imcomplement(label_P).*Y);
+Fn_4L = sum(imcomplement(label_L).*Y);
+Fn_4G = sum(imcomplement(label_G).*Y);
+Fn_4P = sum(imcomplement(label_P).*Y);
+
+% Accuracy(ACC) = (Tp+Tn)/(Tp+Tn+Fp+Fn)
+acc_linear =  (Tp_4L+Tn_4L)/(Tp_4L +Tn_4L +Fp_4L +Fn_4L);
+acc_gaussian = (Tp_4G+Tn_4G)/(Tp_4G +Tn_4G +Fp_4G +Fn_4G);
+acc_polynomial = (Tp_4P+Tn_4P)/(Tp_4P +Tn_4P +Fp_4P +Fn_4P);
+
+% Precision (positive predictive value (PPV)) = Tp/(Tp+Fp)
+ppv_linear = Tp_4L/(Tp_4L+Fp_4L);
+ppv_gaussian = Tp_4G/(Tp_4G+Fp_4G);
+ppv_polynomial = Tp_4P/(Tp_4P+Fp_4P);
+
+% Sensitivity, Recall, HitRate, True Positive rate = Tp/(Tp+Fn)
+sen_linear = Tp_4L/(Tp_4L+Fn_4L);
+sen_gaussian = Tp_4G/(Tp_4G+Fn_4G);
+sen_polynomial = Tp_4P/(Tp_4P+Fn_4P);
+
+% specificity or true negative rate (TNR) = Tn/(Tn+Fp)
+spci_linear = Tn_4L/(Tn_4L+Fp_4L);
+spci_gaussian = Tn_4G/(Tn_4G+Fp_4G);
+spci_polynomial = Tn_4P/(Tn_4P+Fp_4P);
+
+disp('Linear SVM Kernel:');
+disp(['Accuracy = ' num2str(acc_linear)]);
+disp(['Precision = ' num2str(ppv_linear)]);
+disp(['Sensitivity/Recall = ' num2str(sen_linear)]);
+disp(['Specificity = ' num2str(spci_linear)]);
+
+
+
